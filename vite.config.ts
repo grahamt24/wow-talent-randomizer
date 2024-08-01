dfd34d3037475784452fe922fa6e8e99ac6ff245
Graham Thomas <41344647+grahamt24@users.noreply.github.com>
2024-08-01 17:26:36 -0600

    Update base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/df-talent-randomizer",
   server: {
     open: true,
     port: 3000,
